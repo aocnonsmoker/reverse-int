0019d235666773689409190f17ce2fcff29305a9
Zhambyl Kairken <z.kairken@csi.kz>
2021-08-19 19:28:52 +0600

    reversed number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    const numberArr = String(n).split('');
+    let result = ''
+    for (let i = numberArr.length - 1; i > -1; --i) {
+        if (numberArr[i] !== '-') {
+            result += numberArr[i];
+        }
+    }
+    return Number(result);
 }
